da6e86032f0e2bae1a5484c91f115a01e700edb6
Jim Platania <skippylafarge@protonmail.com>
2020-07-07 08:21:40 -0400

    wip

--- ctfscan.sh
+++ ctfscan.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+TARGET=$1
+
+NMAP_TCP_CMD="nmap -v -p- -sC -sV -oA nmap-tcp-all-ports $TARGET | tee nmap-tcp-all-ports.tee
+"
+
+screen -t nmap_tcp
+NMAP_TCP_RESULTS=$(screen -p nmap_tcp -X exec $NMAP_TCP_CMD)
+
+OPEN_TCP_SERVICES=$(grep open nmap-tcp-all-ports.nmap | grep tcp)
+
+OPEN_HTTP_PORTS=$(grep "tcp\&open" $NMAP_TCP_RESULTS | cut -d '/' -f 1)
+
+for http_port in $OPEN_HTTP_PORTS; do
+    gobuster dir -w /usr/share/wordlists/seclists/Discovery/Web-Content/common.txt -u $TARGET:$http_port | tee gobuster-common-port$http_port
+done
 
 
-NMAP_TCP_CMD="nmap -v -sC -sV -oA"
 
-run_nmap_tcp() {
-    screen -t nmap_tcp
-    screen -p nmap_tcp -X exec nmap
-}
+    
