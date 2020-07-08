698e1464f6cbf4caf591de9fff98232ac8e8bfee
Jim Platania <skippylafarge@protonmail.com>
2020-07-07 22:18:32 -0400

    wip

--- ctfscan.sh
+++ ctfscan.sh
@@ -2,20 +2,26 @@
 
 TARGET=$1
 
-NMAP_TCP_CMD="nmap -v -p- -sC -sV -oA nmap-tcp-all-ports $TARGET | tee nmap-tcp-all-ports.tee
+NMAP_TCP_CMD="nmap -v -p- -sC -sV -oA nmap-tcp-all-ports $TARGET
 "
 
 screen -t nmap_tcp
-NMAP_TCP_RESULTS=$(screen -p nmap_tcp -X exec $NMAP_TCP_CMD)
 
-OPEN_TCP_SERVICES=$(grep open nmap-tcp-all-ports.nmap | grep tcp)
+wait
 
-OPEN_HTTP_PORTS=$(grep "tcp\&open" $NMAP_TCP_RESULTS | cut -d '/' -f 1)
+screen -p nmap_tcp -X stuff "$NMAP_TCP_CMD"
 
-for http_port in $OPEN_HTTP_PORTS; do
-    gobuster dir -w /usr/share/wordlists/seclists/Discovery/Web-Content/common.txt -u $TARGET:$http_port | tee gobuster-common-port$http_port
-done
+wait
 
+grep open nmap-tcp-all-ports.nmap > grep-open-tcp-services
 
+wait
 
-    
+grep http grep-open-tcp-services | cut -d '/' -f 1 > open-http-ports
+
+wait
+
+# for http_port in $OPEN_HTTP_PORTS; do
+#     gobuster dir -w /usr/share/wordlists/seclists/Discovery/Web-Content/common.txt -u $TARGET:$http_port | tee gobuster-common-port$http_port
+# done
+
